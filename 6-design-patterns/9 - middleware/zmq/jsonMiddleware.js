9831fd5ce724892f7b45cc4494cb03417fa5d2b1
danrleyt <danrleyt@gmail.com>
2020-07-15 14:42:12 +0200

    adding zmq examples using middlewares

---
+++ 6-design-patterns/9 - middleware/zmq/jsonMiddleware.js
@@ -0,0 +1,12 @@
+module.exports.json = () => {
+  return {
+    inbound: function(message, next) {
+      message.data = JSON.parse(message.data.toString())
+      next()
+    },
+    outbound: function(message, next) {
+      message.data = new Buffer(JSON.stringify(message.data))
+      next()
+    }
+  }
+}
